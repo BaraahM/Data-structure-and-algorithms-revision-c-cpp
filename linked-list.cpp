3f40d1e4ed4e32d11d37dff4e0bfe38ab8ab6257
BARAAH ALAWI AHMED MEKYASH <BaraahMekyash@gmail.com>
2024-02-24 06:52:20 +0800

    Create linked-list.cpp

---
+++ linked-list.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+Struct Node{
+    int data; // 4bytes for data
+    node* next; // 4 bytes for address 6add->5add->2add"0"
+};
+
+class LinkedList{
+    private:
+    Node* head; // Pointer to the first node in the list}
+}
+int main(){
+    
+}
